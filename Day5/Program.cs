0fd81987caf6dc489b082708f08e9223fd376d8f
runarer <95697611+runarer@users.noreply.github.com>
2025-12-07 13:50:02 +0100

    Solves day 5 part 1

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,4 +1,6 @@
-﻿string[] lines;
+﻿using System.Collections.Generic;
+
+string[] lines;
 try
 {
     lines = File.ReadAllLines(args[0]);
@@ -11,7 +13,11 @@ try
 var (ingredients, ranges) = ParseIngredientsList(lines);
 
 Console.WriteLine($"Part 1: {CountFreshIngredients(ingredients,ranges)}");
-//Console.WriteLine($"Part 2: {}");
+
+List<(long, long)> nonOverlappingRanges = GetNonOverlappingRanges(ranges);
+long totalFresh = nonOverlappingRanges.Sum(i => i.Item2 - i.Item1 + 1);
+
+Console.WriteLine($"Part 2: {totalFresh}");
 
 return 0;
 
@@ -58,3 +64,57 @@ int CountFreshIngredients(List<long> ingredients, List<(long, long)> ranges)
     return freshIngredients;
 }
 
+List<(long, long)> GetNonOverlappingRanges(List<(long, long)> ranges)
+{
+    List<(long, long)> nonOverlappingRanges = [];
+
+    foreach (var range in ranges)
+    {
+        long start = range.Item1;
+        long end = range.Item2;
+
+        bool insideAnother = false;
+
+        for (var i = 0; i < nonOverlappingRanges.Count; i++)
+        {
+            long curStart = nonOverlappingRanges[i].Item1;
+            long curEnd   = nonOverlappingRanges[i].Item2;
+
+            // If new range is inside of current, skip new range
+            if (start >= curStart && end <= curEnd)
+            {
+                insideAnother = true;
+                break;
+            }
+
+            // If current is inside of new range, delete current
+            if (start <=  curStart &&  end >= curEnd)
+            {
+                nonOverlappingRanges.RemoveAt(i);
+                i--;
+                continue;
+            }
+
+            // If current is oveerlapping on left side
+            if( start < curStart && end >= curStart && end <= curEnd)
+            {
+                end = curStart-1;
+                continue;
+            }
+
+            // If current is oveerlapping on right side
+            if (start >= curStart && start <= curEnd && end > curEnd)
+            {
+                start = curEnd + 1;
+                continue;
+            }
+        }
+        
+        if(!insideAnother)
+        {
+            nonOverlappingRanges.Add((start,end));
+        }
+    }
+
+    return nonOverlappingRanges;
+}

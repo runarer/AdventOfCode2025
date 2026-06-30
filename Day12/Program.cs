bb1611bbbde4fd327e3db3bfb97b869b8ac0e2ae
runarer <95697611+runarer@users.noreply.github.com>
2026-06-30 11:25:13 +0200

    Solves part 1 day 12

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Text.RegularExpressions;
+﻿/*
+ This was way simpler than expected.
+ */
+
+using System.Text.RegularExpressions;
 
 string[] lines = [];
 
@@ -12,14 +16,97 @@ catch (Exception ex)
     return 1;
 }
 
-List<Puzzle> puzzles = [];
+List<Puzzle> unsolved = [];
 
 foreach(string line in lines.Skip(30))
 {
-    puzzles.Add(ParseLine(line));
+    unsolved.Add(ParseLine(line));
 }
 
+
+List<Puzzle> solved = [];
+List<Puzzle> unsolvable = [];
+
+(unsolved, solved,unsolvable) = FindObviousSolutions(unsolved, solved);
+//(unsolved, solved, unsolvable) = FindSimpleSolutions(unsolved, solved, unsolvable);
+
+Console.WriteLine($"Part 1: {unsolved.Count}");
+
+
 return 0;
+
+
+static (List<Puzzle>, List<Puzzle>, List<Puzzle>) FindObviousSolutions(List<Puzzle> unsolved, List<Puzzle> solved)
+{
+    List<Puzzle> notSolved = [];
+    List<Puzzle> unsolvable = [];
+
+    // If all pieces can be placed in a 3x3 cell
+    foreach(Puzzle p in unsolved)
+    {
+        // find grid of 3x3 cells
+        int grids = ( (p.Height / 3) * (p.Width/3) ) / 9;
+        int pieces = p.PresentZero + p.PresentOne + p.PresentTwo + p.PresentThree + p.PresentFour +p.PresentFive;
+
+        int gridTiles = p.Height * p.Width;
+        int piecesTiles = 7*p.PresentZero + 5*p.PresentOne + 7*p.PresentTwo + 7*p.PresentThree + 7*p.PresentFour + 6*p.PresentFive;
+
+        // If all pieces can be placed in its own 3x3 cell
+        if (grids >= pieces) 
+            solved.Add(p);
+        
+        // If there is not enough cells in the grid to fit all the tiles, there can not be any solutions
+        else if(gridTiles < piecesTiles)
+            unsolvable.Add(p);
+
+        // Need another method
+        else
+            notSolved.Add(p);
+    }
+
+    return (notSolved,solved,unsolvable);
+}
+
+// Turns out this was not needed!
+static (List<Puzzle>, List<Puzzle>, List<Puzzle>) FindSimpleSolutions(List<Puzzle> unsolved, List<Puzzle> solved, List<Puzzle> unsolvable)
+{
+    List<Puzzle> notSolved = [];
+
+    // If all pieces can be placed in a 3x3 cell
+    foreach (Puzzle p in unsolved)
+    {
+        int sixHeight = p.Height / 6;
+        int sixWidth = p.Width / 6;
+        int sixCells = sixHeight * sixWidth;
+
+        int leftOnTop = p.Height - (sixHeight*6);
+        int leftOnRight = p.Width - (sixWidth*6);
+
+        int oneTwoFiveSquares = Math.Min(p.PresentOne / 2, p.PresentTwo / 2);
+        oneTwoFiveSquares = Math.Min(oneTwoFiveSquares, p.PresentFive / 2);
+
+        int oneTwoFiveSquaresPlaced = Math.Min(oneTwoFiveSquares,sixCells);
+        sixCells = sixCells-oneTwoFiveSquaresPlaced;
+
+        Console.WriteLine(sixCells);
+
+        // 3x3 cells left 
+        int threeByThreeCells = sixCells * 4 + ((leftOnTop / 3) * (p.Width/3) ) + ((leftOnRight/3) * (p.Height-leftOnTop));
+
+        int presentsLeft =     p.PresentZero 
+                            + (p.PresentOne - 2*oneTwoFiveSquaresPlaced) 
+                            + (p.PresentTwo - 2 * oneTwoFiveSquaresPlaced)
+                            +  p.PresentThree + p.PresentFour
+                            + (p.PresentFive - 2 *oneTwoFiveSquaresPlaced);
+        if (presentsLeft <= threeByThreeCells)
+            solved.Add(p);
+        else
+            notSolved.Add(p);
+    }
+
+    return (notSolved, solved, unsolvable);
+}
+
 
 
 static Puzzle ParseLine(string line)
@@ -46,4 +133,4 @@ static Puzzle ParseLine(string line)
     throw new Exception("A line did not match regex.");
 }
 
-record Puzzle(int Height, int Width, int Present0, int Present1, int Present2, int Present3, int Presesnt4, int Present5);
+record Puzzle(int Height, int Width, int PresentZero, int PresentOne, int PresentTwo, int PresentThree, int PresentFour, int PresentFive);
